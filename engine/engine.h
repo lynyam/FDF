ba910505ecddc9ca804c7e22e803289c981c4bf1
Leon NYAMETSO <lnyamets@made-f0Ar6s7.clusters.42paris.fr>
2023-10-13 17:09:02 +0200

    add modify

--- engine/engine.h
+++ engine/engine.h
@@ -6,7 +6,7 @@
 /*   By: lnyamets <lnyamets@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 18:47:03 by lnyamets          #+#    #+#             */
-/*   Updated: 2023/10/11 20:17:04 by lnyamets         ###   ########.fr       */
+/*   Updated: 2023/10/13 16:58:51 by lnyamets         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ typedef struct	s_window {
 }				t_window;
 
 void	engine_connect_x_server(t_window *p_window);
+void	engine_create_window(t_window *p_window);
+t_pair engine_espace_to_iso(int p1_x, int p1_y, int p2_x, int p2_y, t_matrix *p_matrix);
 void	engine_create_window(t_window *p_window);
 t_pair engine_espace_to_iso(int p1_x, int p1_y, int p2_x, int p2_y, t_matrix *p_matrix);
 
@@ -70,6 +72,14 @@ void	engine_create_window(t_window *p_window);
 void	engine_draw(t_matrix *p_matrix, t_window *p_window);
 t_pair engine_espace_to_iso(int p1_x, int p1_y, int p2_x, int p2_y, t_matrix *p_matrix);
 void engine_plot_line(t_pair *p_pair, t_window *p_window);
+void	draw_line_slope(t_window *p_window, t_pair *p_pair, int dx, int dy, int incX, int incY);
+void	draw_line_horizontal(t_window *p_window, t_pair *p_pair, int incX);
+void	draw_slope(t_window *p_window, int x0, int y0, int x1, int y1, int dx, int dy, int incX, int incY);
+void	draw_horizontal_line(t_window *p_window, int x0, int x1, int y, int c, int incX);
+int	color_to_int(struct s_color *p_color);
+int	engine_close_window(void *param);
+void    engine_callback_hook(t_window *p_window);
+
 void	draw_line_slope(t_window *p_window, t_pair *p_pair, int dx, int dy, int incX, int incY);
 void	draw_line_horizontal(t_window *p_window, t_pair *p_pair, int incX);
 void	draw_slope(t_window *p_window, int x0, int y0, int x1, int y1, int dx, int dy, int incX, int incY);
